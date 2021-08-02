95d062174cbd67082c39056908d846f3f7642cad
ValentinVernier <63792187+ValentinVernier@users.noreply.github.com>
2021-08-02 11:19:50 +0200

    Modif header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { OrderComponent } from './order/order.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    SidebarComponent,
+    OrderComponent,
     
   ],
   imports: [
